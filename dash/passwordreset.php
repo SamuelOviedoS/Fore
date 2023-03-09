9067048c0358f551e9f7fb129e77e8ae96e241fa
SamuelOviedoS <oviedosamuel0721@gmail.com>
2023-03-09 15:57:03 -0600

    style img

--- dash/passwordreset.php
+++ dash/passwordreset.php
@@ -37,7 +37,7 @@
     </div>
 
     <div class="responsive">
-        <img src="../resources/img/support.jpg" alt="support">
+        <img src="../resources/img/support.jpg" alt="soporte">
     </div>
 </body>
 
