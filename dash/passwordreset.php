9e8eb92fde61c04f7c55b175902c6bd5c032f9b0
SamuelOviedoS <oviedosamuel0721@gmail.com>
2023-03-09 15:31:48 -0600

    pagina contraseña perdida

--- dash/passwordreset.php
+++ dash/passwordreset.php
@@ -3,13 +3,41 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap');
+    </style>
+    <link rel="stylesheet" href="../css/passwd.css">
     <title>Soporte</title>
+    <link rel="shortcut icon" href="https://erp.unacifor.edu.hn/img/logout.png">
 </head>
+
 <body>
-    
+    <div class="header">
+        <div class="displayflex">
+            <div class="item">
+                <div>
+                    <span class="site-logo-img">
+                        <a href="https://unacifor.edu.hn/">
+                            <img src="https://erp.unacifor.edu.hn/img/logout.png" alt="siteLogo">
+                        </a>
+                    </span>
+                </div>
+                <div>
+                    <h2>
+                        UNACIFOR | SIU
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="responsive">
+    </div>
 </body>
+
 </html>
