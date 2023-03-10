90b2d7f364a2528148cd39ebde6e1368afc69be1
SamuelOviedoS <oviedosamuel0721@gmail.com>
2023-03-10 09:35:20 -0600

    rediseño btnvolver

--- dash/passwordreset.php
+++ dash/passwordreset.php
@@ -39,28 +39,34 @@
             <div class="cont text-content">
                 <h3>Contactanos</h3>
                 <p>
-                    <strong>Si no recuerdas tu contraseña</strong>, favor ponerse en contacto con soporte
-                    técnico.
+                    <strong>Si no recuerdas tu contraseña</strong>, favor ponerse en
+                    contacto con soporte técnico.
                 </p>
                 <a href="https://mail.google.com/mail/?view=cm&to=soporte@unacifor.edu.hn&su=Restablecimiento%20de%20contraseña&body=Hola,%20he%20perdido%20mi%20contraseña%20mi%20usuario%20es:">Enviar correo a:
                     <strong>soporte@unacifor.edu.hn</strong>
                 </a>
+                <div class="content-btn">
+                    <button type="button" class="btnupd" id="upd-pwsd" onclick="back()">
+                        <span class="btn-icon">
+                            <ion-icon name="arrow-back-circle-outline"></ion-icon>
+                        </span>
+                        <span class="btn-text">Volver</span>
+                    </button>
+                </div>
             </div>
             <div class="cont img-content">
                 <img src="../resources/img/support.jpg" alt="support" />
-            </div>
-            <div class="content-btn">
-                <button type="button" class="btnupd" id="upd-pwsd" onclick="updpassbtn()">
-                    <span class="btn-text">Volver</span>
-                    <span class="btn-icon">
-                        <ion-icon name="refresh-circle-outline"></ion-icon>
-                    </span>
-                </button>
             </div>
         </div>
     </div>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script>
+        function back() {
+            window.location.href = "../index.php";
+
+        }
+    </script>
 </body>
 
 </html>
